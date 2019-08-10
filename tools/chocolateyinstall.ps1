645505d9e680486b93f3311e27c0a160b0df5255
Andrei Hutanu <andrei.hutanu@evalan.com>
2019-08-10 12:47:45 +0200

    without ahk overhead

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,6 +1,5 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-. "$(Join-Path $toolsDir chocolateyhelper.ps1)"
 . "$(Join-Path $toolsDir commonEnv.ps1)"
 
 $url        = 'https://www.nordicsemi.com/-/media/Software-and-other-downloads/Desktop-software/nRF-command-line-tools/sw/Versions-10-x-x/nRF-Command-Line-Tools_10_2_1_Installer_64.exe'
@@ -12,16 +11,10 @@ $packageArgs = @{
   softwareName  = 'nrfjprog*'
   checksum      = 'B707BB13D69DC8DC211AE4F43EFA72A36FEDD8B36E610C17548E01E9414E1DA0'
   checksumType  = 'sha256'
-  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  silentArgs    = "/passive /quiet /norestart /log `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes= @(0, 3010, 1641)
 }
-$ahkProc = Start-Ahk('chocolateyhelper.ahk')
-
 Install-ChocolateyPackage @packageArgs
 
 Install-ChocolateyPath "$JLINK_BIN_PATH" -PathType 'Machine'
 Install-ChocolateyPath "$NRF_BIN_PATH" -PathType 'Machine'
-
-# minimum delay to ensure the autohotkey takes efect
-& Start-Sleep 1
-if (Get-Process -id $ahkProc.Id -ErrorAction SilentlyContinue) {Stop-Process -id $ahkProc.Id}

b8e954f61aba7b2b2b47c23329facd31b0286297
Andrei <n3rd4i@gmail.com>
2019-09-02 18:50:12 +0200

    update to version 10.3.0

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,14 +2,13 @@
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 . "$(Join-Path $toolsDir commonEnv.ps1)"
 
-$url        = 'https://www.nordicsemi.com/-/media/Software-and-other-downloads/Desktop-software/nRF-command-line-tools/sw/Versions-10-x-x/nRF-Command-Line-Tools_10_2_1_Installer_64.exe'
+$url        = 'https://www.nordicsemi.com/-/media/Software-and-other-downloads/Desktop-software/nRF-command-line-tools/sw/Versions-10-x-x/nRF-Command-Line-Tools_10_3_0_Installer_64.exe'
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
   fileType      = 'EXE'
   url           = $url
   softwareName  = 'nrfjprog*'
-  checksum      = 'B707BB13D69DC8DC211AE4F43EFA72A36FEDD8B36E610C17548E01E9414E1DA0'
+  checksum      = '3BD892FB54DF3E9F1230FFC7FAC268C960A9A8179FB82F1D70FBBE7BC54FB432'
   checksumType  = 'sha256'
   silentArgs    = "/passive /quiet /norestart /log `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes= @(0, 3010, 1641)

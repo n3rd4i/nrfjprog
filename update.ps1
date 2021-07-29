2403efc9c0b263d7bd84ee402a188cfc845fe14a
Andrei Hutanu <andrei.hutanu@evalan.com>
2021-07-29 18:08:11 +0200

    hotfix: updated the auto-update script with latest changes

--- update.ps1
+++ update.ps1
@@ -1,7 +1,7 @@
 import-module au
 
 $domain   = 'https://www.nordicsemi.com'
-$releases = "$domain/Software-and-tools/Development-Tools/nRF-Command-Line-Tools/Download#infotabs"
+$releases = "$domain/Software-and-tools/Development-Tools/nRF-Command-Line-Tools/Download"
 
 function global:au_SearchReplace {
   @{
@@ -15,11 +15,11 @@ function global:au_GetLatest {
   $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing #1
   $regex    = 'sc_DownloadFiles_CommandLineTools'
   $url_raw  = $download_page.links | ? id -match $regex | select -First 1 -expand href
-  $url_x64  = $url_raw -replace '.exe','_64.exe'
+  $url_x64  = $url_raw -replace '_WIN32.zip','_WIN64.zip'
   $url      = ($domain, $url_x64) -Join('')
 
   $token = $url -split 'nRF-Command-Line-Tools_' | select -First 1 -Skip 1 #3
-  $raw_version = $token -split '_Installer_64.exe' | select -Last 1 -Skip 1 #3
+  $raw_version = $token -split '_WIN64.zip' | select -Last 1 -Skip 1 #3
   $version = $raw_version -replace '_','.'
   return @{ Version = $version; URL32 = $url }
 }
